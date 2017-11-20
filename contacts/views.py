712703c29b4c002fb9c4d445ef29a730639b1ec0
Muhammad Hannan <hannansultan@outlook.com>
2017-11-21 01:12:00 +0500

    loaded data from db

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from contacts.models import Contacts
 
 
 def default(request):
     return render(request, 'contacts/input.html')
+
+
+def search(request):
+    all_contacts = Contacts.objects.all().order_by("name")[:50]
+    template = loader.get_template('contacts/search_result.html')
+    context = {
+        'all_contacts': all_contacts,
+    }
+
+    return HttpResponse(template.render(context, request))
